2768ba02c9424787309297afd2973b813ed7b732
mbadielisha <embadi43@gmail.com>
2024-09-18 02:05:23 -0400

    reinit

--- internal/model/models.go
+++
@@ -1,9 +0,0 @@
-package model
-
-type Donation struct {
-	ID         int    `json:"id"`
-	FoodType   string `json:"food_type"`
-	Quantity   int    `json:"quantity"`
-	Expiration string `json:"expiration"`
-	Location   string `json:"location"`
-}
--- internal/storage/queries/queries.sql
+++
@@ -1,7 +0,0 @@
--- name: CreateDonation :one
-INSERT INTO donations (food_type, quantity, expiration, location)
-VALUES ($1, $2, $3, $4)
-RETURNING id;
-
--- name: GetDonations :many
-SELECT id, food_type, quantity, expiration, location FROM donations;
