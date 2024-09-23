6ec9ab6779a490ce680c42522cbb3d1cace3a396
mbadielisha <embadi43@gmail.com>
2024-09-23 06:00:47 -0400

    add test userRepository:findAllUsers

--- repositories/userRepository_test.go
+++ repositories/userRepository_test.go
@@ -1,6 +1,11 @@
 package repository_test
 
 import (
+	"testing"
+
+	"github.com/elishambadi/sharebite/models"
+	repository "github.com/elishambadi/sharebite/repositories"
+	"github.com/stretchr/testify/assert"
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
 )
@@ -8,4 +13,23 @@ import (
 func setupTestDB() (*gorm.DB, error) {
 	// SQLite in-memory database for testing
 	return gorm.Open(sqlite.Open(":memory:"), &gorm.Config{})
+}
+
+func TestFindAllUsers(t *testing.T) {
+	db, err := setupTestDB()
+	assert.NoError(t, err)
+
+	db.AutoMigrate(&models.User{})
+
+	fakeUsers := models.FakeUsers()
+
+	for _, user := range fakeUsers {
+		db.Create(&user)
+	}
+
+	userRepo := repository.NewGormUserRepository(db)
+
+	users, err := userRepo.FindAll()
+	assert.NoError(t, err)
+	assert.Equal(t, fakeUsers[0].Email, users[0].Email)
 }
